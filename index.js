ee0bf9cea4246554478f2ed2c97e26c0a194bb2e
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-08 12:44:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+const recipes = {}
+
+function updateObjectWithKeyAndValue(object,key,value) {
+  var newobj = Object.assign({},object,{key:value})
+  return newobj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  object[key] = value
+  return object
+}
